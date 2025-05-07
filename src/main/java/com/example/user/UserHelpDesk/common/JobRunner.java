d3386287872f762adcfda679d234cd097dc5b9a2
NaveenKumar <naveenkumar.a@sparkouttech.com>
2025-05-07 11:41:25 +0530

    checking ci and cd

--- src/main/java/com/example/user/UserHelpDesk/common/JobRunner.java
+++ src/main/java/com/example/user/UserHelpDesk/common/JobRunner.java
@@ -1,29 +1,29 @@
-package com.example.user.UserHelpDesk.common;
-
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobParameters;
-import org.springframework.batch.core.JobParametersBuilder;
-import org.springframework.batch.core.launch.JobLauncher;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.stereotype.Component;
-
-@Component
-public class JobRunner implements CommandLineRunner {
-
-    @Autowired
-    private JobLauncher jobLauncher;
-
-    @Autowired
-    private Job job;
-
-    @Override
-    public void run(String... args) throws Exception {
-        JobParameters parameters = new JobParametersBuilder()
-                .addLong("startAt", System.currentTimeMillis())
-                .toJobParameters();
-
-        jobLauncher.run(job, parameters);
-    }
-}
-
+//package com.example.user.UserHelpDesk.common;
+//
+//import org.springframework.batch.core.Job;
+//import org.springframework.batch.core.JobParameters;
+//import org.springframework.batch.core.JobParametersBuilder;
+//import org.springframework.batch.core.launch.JobLauncher;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//public class JobRunner implements CommandLineRunner {
+//
+//    @Autowired
+//    private JobLauncher jobLauncher;
+//
+//    @Autowired
+//    private Job job;
+//
+//    @Override
+//    public void run(String... args) throws Exception {
+//        JobParameters parameters = new JobParametersBuilder()
+//                .addLong("startAt", System.currentTimeMillis())
+//                .toJobParameters();
+//
+//        jobLauncher.run(job, parameters);
+//    }
+//}
+//
